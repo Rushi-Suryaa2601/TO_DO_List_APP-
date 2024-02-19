fbb91853a01b4a5db44515c9408969b14b379faa
Rushikesh Suryagandh <rushisuryagandh4455@gmail.com>
2024-02-19 16:00:48 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,73 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Navbar from './components/Navbar'
+import { v4 as uuidv4 } from 'uuid';
+// ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
 
 
 function App() {
+const [todo, settodo] = useState('')
+const [todos, settodos] = useState([])
+
+  const handleAdd=() => { 
+    settodos([...todos,{id:uuidv4(),todo,isCompleted:false}])
+    settodo("")
+    console.log(todos)
+   }
+  const handleEdit=() => { 
+    
+   }
+  const handleDelete=() => { 
+   
+   }
+
+   const handleChange=(e) => { 
+    settodo(e.target.value)
+    }
+    
+   const handleCheckbox=(e) => { 
+    let id=e.target.name;
+    console.log(id)
+    let index=todos.findIndex(item=>
+      {
+        return item.id===id;
+      })
+      console.log(index)
+      let newTodos=[...todos];
+      newTodos[index].isCompleted=!newTodos[index].isCompleted;
+      settodos(newTodos)
+    }
   
   return (
     <>
-    <Navbar/>
-      <div className="container ">
-        <div className="bg-red-600">Hey i am red</div>
+    <Navbar/> 
+     <div className="container bg-purple-200 p-5 font-bold my-5 rounded-xl mx-auto min-h-[80vh]">
+      <div className="addTodo my-5">
+        <h1 className='text-lg font-bold'>Add a Todo</h1>
+      <input type="text" className='w-80 outline-none' onChange={handleChange} value={todo} />
+      <button onClick={handleAdd} className='bg-purple-700 text-white p-1 rounded-md text-sm mx-3 cursor-pointer hover:bg-purple-900'>Add Todo</button>
+      </div>
+      <h1 className='text-xl font-bold'>YOUR TODOS</h1>
+
+      <div className="todos">
+        {todos.map(item=>{
+        return<div key={item.id} className="todo flex w-1/2  my-3 justify-between">
+          <input onChange={handleCheckbox} type="checkbox"  value={item.isCompleted} name={item.id} id=''/>
+          <div className={item.isCompleted? "line-through":""}>
+            <div className='text-xl'>
+            {item.todo}
+            </div>
+
+            </div>
+          <div className="buttons">
+            <button onClick={handleEdit} className='bg-purple-700 text-white p-2 rounded-md text-sm mx-2 cursor-pointer hover:bg-purple-900'>Edit</button>
+            <button onClick={handleDelete} className='bg-purple-700 text-white p-2 rounded-md text-sm mx-2 cursor-pointer hover:bg-purple-900'>Delete</button>
+          </div>
+
         </div>
+        })}
+      </div>
+     </div>
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-      Hey i am Navbar
-    </div>
+    <>
+      <nav className='flex justify-between bg-purple-700 text-white p-2  '>
+        <div className="logo">
+          <span className='font-bold text-xl mx-9'>MYTODO</span>
+        </div>
+        <ul className='flex gap-10 mx-9'> 
+          <li className='cursor-pointer hover:font-bold transition-all duration-75'>Home</li>
+          <li className='cursor-pointer hover:font-bold transition-all duration-75'>Your Tasks</li>
+          <li></li>
+        </ul>
+      </nav>
+    </>
   )
 }
 
